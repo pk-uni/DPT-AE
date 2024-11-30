76297e02af3686be4715e4c00684f15dfe05f269
Petros <93052465+pkitazos@users.noreply.github.com>
2024-11-30 12:01:22 +0000

    set parallel experiment values

--- erlang/src/test_parallel.erl
+++ erlang/src/test_parallel.erl
@@ -5,13 +5,13 @@
 -define(NUM_RUNS, 5).
 
 run_DS1() ->
-    run_parallel_test("DS1", 1, 15000, [1, 2, 4, 8]).
+    run_parallel_test("DS1", 1, 15000, [1, 2, 4, 8, 12, 16, 24, 32, 48, 64]).
 
 run_DS2() ->
-    run_parallel_test("DS2", 1, 30000, [1, 2, 4, 8]).
+    run_parallel_test("DS2", 1, 30000, [1, 2, 4, 8, 12, 16, 24, 32, 48, 64]).
 
 run_DS3() ->
-    run_parallel_test("DS3", 1, 6000, [16, 32]).
+    run_parallel_test("DS3", 1, 60000, [2, 4, 8, 12, 16, 24, 32, 48, 64]).
 
 run_parallel_test(Dataset, Lower, Upper, WorkerCounts) ->
     % io:format("Running parallel tests for Dataset ~p (range: ~p to ~p)~n", [Dataset, Lower, Upper]),
